79fcb499c8697f8fe8b7aad40b69fc9dd784a09a
Dinesh Pawar <dsp70458@gmail.com>
2025-04-24 22:33:26 +0530

    fix: update API base URL to use production environment

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "https://book-review-platform-dzkb.onrender.com/api",
   withCredentials: true,
 });
 
